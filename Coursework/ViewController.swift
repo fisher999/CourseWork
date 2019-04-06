85a3696b1054163fb0f1c1db9745294c2cf207f3
semeeenov_viktor <vsemenov@asdgroup.pro>
2019-04-06 23:03:15 +0700

    Initial Commit

---
+++ Coursework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coursework
+//
+//  Created by Victor on 06/04/2019.
+//  Copyright © 2019 Victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

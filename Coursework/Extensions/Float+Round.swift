6afef40d445263d226f4e06f336f735d1569f47b
semeeenov_viktor <vsemenov@asdgroup.pro>
2019-04-26 00:07:06 +0700

    Removing feedbacks

---
+++ Coursework/Extensions/Float+Round.swift
@@ -0,0 +1,9 @@
+//
+//  Floar+Round.swift
+//  Coursework
+//
+//  Created by Victor on 25/04/2019.
+//  Copyright © 2019 Victor. All rights reserved.
+//
+
+import Foundation
---
+++ Coursework/Model/MDResponse.swift
@@ -0,0 +1,9 @@
+//
+//  MDResponse.swift
+//  Coursework
+//
+//  Created by Victor on 25/04/2019.
+//  Copyright © 2019 Victor. All rights reserved.
+//
+
+import Foundation
